faf724a422a4e35781efc7fb93687bc1256d6933
ankabutdev <Ankabut571@gmail.com>
2024-01-08 01:04:31 +0500

    learn some templetes

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Navbar2Component } from './navbar2/navbar2.component';
 import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -13,5 +16,8 @@ export const routes: Routes = [
     { path: 'navbar', component: NavbarComponent },
     { path: 'navbar2', component: Navbar2Component },
     { path: 'home-navbar', component: HomeNavbarComponent },
+    { path: 'sign-up', component: SignUpComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', component: NotFoundComponent }
 ];
---
+++ demo/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+})
+export class SignUpComponent implements OnInit {
+  async save() {
+    await alert('saved');
+  }
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
